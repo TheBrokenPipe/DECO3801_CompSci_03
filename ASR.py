36eb428c5223c43e93755d126601007934af8c80
LordVenter <rieckertventer@gmail.com>
2024-08-23 11:42:56 +1000

    latest

--- ASR.py
+++ ASR.py
@@ -8,11 +8,14 @@ class ASR:
     def __init__(self, open_ai_client: OpenAI):
         self.open_ai_client = open_ai_client
 
+    def transcribe_audio(self, audio_file):
+        return self.open_ai_client.audio.transcriptions.create(
+            model=self.open_ai_asr_model,
+            file=audio_file
+        ).text
+
     def transcribe_audio_file(self, file_path: str) -> str:
         """from path"""
         with open(file_path, "rb") as audio_file:
-            transcription = self.open_ai_client.audio.transcriptions.create(
-                model=self.open_ai_asr_model,
-                file=audio_file
-            )
-        return transcription.text
+            transcription = self.transcribe_audio(audio_file)
+        return transcription
