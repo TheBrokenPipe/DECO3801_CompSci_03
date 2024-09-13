2594900c0ba2d753a14464b62a4725b693097d4e
Wadzanai-C <161273886+Wadzanai-C@users.noreply.github.com>
2024-09-13 10:24:28 +1000

    Update upload_meeting.py
    this page currently allows you to add meeting recordings. The next button is not working yet

---
+++ frontend/pages/upload_meeting.py
@@ -0,0 +1,36 @@
+# # allowing to work with the intergace in parent directory
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import streamlit as st
+
+     
+col1, col2 = st.columns([6, 1], gap = "large", vertical_alignment="center")
+with col1:
+    st.button('← Back')
+
+with col2:
+    st.button('Help')
+
+st.write('Welcome! To start uploading your meeting, please give the meeting a unique name and upload the transcript/recording from your computer')
+
+meeting_name = st.text_input("Enter the name of the meeting:")
+
+uploaded_file = st.file_uploader("Upload Recording/Transcript", type=['mp3', 'mp4', 'txt', 'wav'])
+
+if uploaded_file is not None:
+    st.write(f"{meeting_name} uploaded successfully.")
+
+col1, col2 = st.columns([6.5, 1], gap = "large", vertical_alignment="top")
+with col1:
+    st.write("")
+
+with col2:
+    st.button("Next", on_click=lambda: st.page_link("upload_meeting2.py"))
+
+# if st.button("Next", key = "next_buttonB"):
+#         st.session_state.page = "upload_page2"
+
+# if st.session_state.page == "upload_page1":
+#     upload_page1()
