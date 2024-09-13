be4aa54371e197d222576f634bdf793c7951b470
Wadzanai-C <161273886+Wadzanai-C@users.noreply.github.com>
2024-09-13 10:25:40 +1000

    Update upload_meeting2.py
    This is the second upload page which allows you to add supporting documents for the meeting like a transcript

--- frontend/pages/upload_meeting2.py
+++ frontend/pages/upload_meeting2.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+col1, col2 = st.columns([6, 1], gap = "large", vertical_alignment="center")
+with col1:
+    st.button('← Back')
+
+with col2:
+    st.button('Help')
+
+st.write('Are there any text documents that are important context for this meeting? Please upload them here')
+
+uploaded_file = st.file_uploader("Upload supporting documents", type=['txt'])
