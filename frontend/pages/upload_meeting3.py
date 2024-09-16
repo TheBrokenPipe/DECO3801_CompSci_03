7085acb05ff8d4f5c525a157a48519a85beddd68
Joanna F <jfind74@gmail.com>
2024-09-16 20:13:56 +1000

    adding a consent button and trying to make it mandatory for a submit. However, there is a known issue.

--- frontend/pages/upload_meeting3.py
+++ frontend/pages/upload_meeting3.py
@@ -3,6 +3,11 @@ import streamlit_tags as stt
 from index import pages
 from interface import server
 
+# known issue: the warning about consent only occurs twice.
+
+if "tried_submit" not in st.session_state:
+    st.session_state["tried_submit"] = False
+
 col1, col2 = st.columns([6, 1], gap="large", vertical_alignment="center")
 with col1:
     if st.button('← Back', key="backTo2"):
@@ -14,7 +19,7 @@ userNames = list(map(lambda user: user.get_name(), server.get_users()))
 # a list of all the topics in the server.
 topicNames = list(map(lambda topic: topic.get_name(), server.get_topics()))
 
-# page content goes here
+# page content
 attendees = stt.st_tags(
     label='Meeting attendees',
     text='Press enter to add more',
@@ -29,6 +34,13 @@ topics = stt.st_tags(
     suggestions=topicNames,
     key="sdwbh")
 
+consent = st.checkbox("The attendees consent to this meeting being uploaded and \
+                      visible to all people can see the tags", key="wevsj")
+
+if st.session_state["tried_submit"]:
+    st.warning("Please get consent from participants or reduce who you're sharing \
+                 the video with.")
+
 col1, col2, col3 = st.columns([1, 4, 1], gap="large", vertical_alignment="top")
 with col1:
     if st.button("← Back", key="previous"):
@@ -37,6 +49,11 @@ with col1:
 with col2:
     st.write("")
 
-with col3:
-    if st.button("Submit", key="submit"):
-        st.switch_page(pages["chat"])
+submit = col3.button("Submit", key="submit")
+
+if submit and consent:
+    st.switch_page(pages["chat"])
+    st.session_state["tried_submit"] = False
+
+if submit and not consent:
+    st.session_state["tried_submit"] = True
