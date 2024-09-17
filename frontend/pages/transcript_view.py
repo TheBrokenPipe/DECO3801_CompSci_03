6b00ab492b40c1dcfc083e3fa99ab88c76c3581e
Joanna F <jfind74@gmail.com>
2024-09-17 11:06:08 +1000

    Filling out a transcript display page.

---
+++ frontend/pages/transcript_view.py
@@ -0,0 +1,49 @@
+import datetime
+import random
+import streamlit as st
+from index import pages
+
+
+# return ordered array of the text paragraphs for the transcript
+def get_para_texts():
+    return ["Hello"] * 6
+
+
+# return ordered array of all the speakers to allign with the paragraphs
+def get_para_speaker_names():
+    return ['Speaker 1', 'Speaker 2', 'Speaker 3', 'Speaker 4', 'Speaker 5',
+            'Speaker 6']
+
+
+def get_meeting_name():
+    return "Meeting name"
+
+
+def get_meeting_date():
+    return datetime.datetime(2024, 1, 1)
+
+
+def header():
+    leftHeader, centreHeader, rightHeader = st.columns(3)
+    header_back = leftHeader.button("Back", key=random.random())
+    centreHeader.header("Logo")
+    header_help = rightHeader.button("Help", key=random.random())
+
+    if header_back:
+        st.switch_page(pages["chat"])
+
+    if header_help:
+        st.switch_page(pages["help"])
+
+
+header()
+
+date_string = datetime.datetime.strftime(get_meeting_date(), "%d/%m/%Y")
+st.header(f"{get_meeting_name()} ({date_string})")
+
+# a list of all (speaker, texts) for this transcript
+paras = zip(get_para_speaker_names(), get_para_texts())
+
+for (speaker, text) in paras:
+    speaker_char = speaker[-1]  # unique char to represent the speaker
+    st.chat_message(speaker_char).markdown(speaker + ": " + text)
