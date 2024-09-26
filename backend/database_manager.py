33af5607588dae9b4b70322c507b87a06f2dfe4a
Daniel-ON <61522267+Daniel-ON@users.noreply.github.com>
2024-09-27 08:28:51 +1000

    Remove redundant or outdated functions from database_manager, plus change verbose flag to use proper logger at debug level

--- backend/database_manager.py
+++ backend/database_manager.py
@@ -5,16 +5,15 @@ from psycopg import sql
 from pydantic import BaseModel
 from access import *
 from models import *
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
+import logging
 
 class DB_Manager:
 
     @staticmethod
-    async def setup_table_structure(verbose=False):
-        if verbose: print("Starting Table Setup.")
+    async def setup_table_structure():
+        logger = logging.getLogger(__name__)
+        logger.debug("Starting Table Setup.")
+
         # table_exists = await AccessBase.db_fetchone(
         #     """
         #     SELECT EXISTS (
@@ -29,7 +28,7 @@ class DB_Manager:
         #     return
 
         await AccessBase.db_execute("CREATE EXTENSION IF NOT EXISTS vector;")
-        if verbose: print("pgvector extension installed successfully.")
+        logger.debug("pgvector extension installed successfully.")
 
         await AccessBase.db_execute(
             f"""
@@ -39,7 +38,8 @@ class DB_Manager:
                 name TEXT,
                 file_recording TEXT,
                 file_transcript TEXT,
-                summary TEXT
+                summary TEXT,
+                status TEXT
             );
 
             CREATE TABLE IF NOT EXISTS key_points (
@@ -56,7 +56,8 @@ class DB_Manager:
 
             CREATE TABLE IF NOT EXISTS tag (
                 id SERIAL PRIMARY KEY,
-                name TEXT
+                name TEXT,
+                last_modified DATE
             ); 
 
             CREATE TABLE IF NOT EXISTS meeting_tag (
@@ -80,69 +81,8 @@ class DB_Manager:
             );
             """
         )
-        if verbose: print("Tables setup successfully.")
-
-    @staticmethod
-    async def get_action_items():
-        # Establish a connection to the PostgreSQL database
-        with psycopg.connect(
-                host=os.getenv("HOSTNAME"),
-                port=os.getenv("PORT"),
-                dbname=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD")
-        ) as conn:
-            # Create a cursor object
-            with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
-                # Execute the SELECT statement
-                cur.execute("SELECT * FROM action_items;")
-
-                # Fetch all rows from the result
-                rows = cur.fetchone()
-
-        return rows
-
-    @staticmethod
-    def insert_model_to_db(model_instance: BaseModel, table_name: str):
-        """
-        Inserts a Pydantic model instance into the specified SQL table.
-
-        :param conn: Database connection object.
-        :param model_instance: Instance of the Pydantic model to be inserted.
-        :param table_name: Name of the SQL table.
-        """
-        # Get field names and values from the Pydantic model instance
-        """
-        data = model_instance.model_dump()
-        print(data)
-        fields = list(data.keys())
-        values = list(data.values())
-        print(values)
-        """
-        fields = model_instance.__fields__.keys()
-        values = [getattr(model_instance, field) for field in fields]
-        print(values)
-        values = [v.model_dump_json() if issubclass(type(v), BaseModel) else v for v in values]
-        print(values)
-
-        # Prepare the SQL query
-        query = sql.SQL("INSERT INTO {table} ({fields}) VALUES ({placeholders})").format(
-            table=sql.Identifier(table_name),
-            fields=sql.SQL(', ').join(map(sql.Identifier, fields)),
-            placeholders=sql.SQL(', ').join(sql.Placeholder() * len(fields))
-        )
-        with psycopg.connect(
-                host=os.getenv("HOSTNAME"),  # Use 'localhost' since we're connecting to the Docker container
-                port=os.getenv("PORT"),  # Port that PostgreSQL is exposed on
-                dbname=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD")
-        ) as conn:
-            # Execute the query
-            with conn.cursor() as cur:
-                cur.execute(query, values)
-                conn.commit()
+        logger.debug("Tables setup successfully.")
 
     @classmethod
-    async def full_setup(cls, verbose=False):
-        await cls.setup_table_structure(verbose)
+    async def full_setup(cls):
+        await cls.setup_table_structure()
