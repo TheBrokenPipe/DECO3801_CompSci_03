ec83199d305bc560271e088397f72af32aa5831c
Daniel-ON <61522267+Daniel-ON@users.noreply.github.com>
2024-09-26 19:52:46 +1000

    Separate ingestion functions into backend file and convert remaining into new main

--- backend/ingestion.py
+++ backend/ingestion.py
@@ -1,26 +1,16 @@
-import asyncio
 import sys
 import os
-from dotenv import load_dotenv
-from backend.docker_manager import DockerManager
-from backend.database_manager import DB_Manager
 import logging
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from models import *
 from access import *
-from backend.RAG_local import RAG
-from backend.ASR import ASR
-
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-
-load_dotenv()
-
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+from .ASR import ASR
+from .manager import Manager
 
 class Ingestion:
     def __init__(self):
-        self.rag = RAG()
+        self.manager = Manager()
         self.asr = ASR(os.environ['HF_TOKEN'])
         self.logger = logging.getLogger(__name__)
 
@@ -31,17 +21,17 @@ class Ingestion:
             with open(meeting.file_transcript, 'r', encoding="utf-8") as file:
                 transcript = file.read()
 
-            full_summary = self.rag.summarise_meeting(transcript)
+            full_summary = self.manager.rag.summarise_meeting(transcript)
             self.logger.debug(full_summary)
 
             meeting.summary = full_summary['abstract_summary']
             await update_table(meeting)
 
             for action_item in full_summary['action_items'].action_items:
-                await self.create_action_item(action_item, meeting)
+                await self.manager.create_action_item(action_item, meeting)
 
             for key_point in full_summary['key_points'].key_points:
-                await self.create_key_point(key_point, meeting)
+                await self.manager.create_key_point(key_point, meeting)
             
             # TODO: RAG chunking and embedding goes here
             
@@ -54,14 +44,3 @@ class Ingestion:
             await update_table(meeting)
     
 
-async def main():
-    with DockerManager(stop_when_done=False) as m:
-        m.full_setup()
-        await DB_Manager.full_setup()
-        ingestion = Ingestion()
-        while True:
-            await ingestion.transcribe_next_meeting()
-            await ingestion.ingest_next_meeting()
-        
-        
-asyncio.run(main())
---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import sys
+from dotenv import load_dotenv
+from backend.docker_manager import DockerManager
+from backend.database_manager import DB_Manager
+import logging
+from models import *
+from access import *
+from backend.ingestion import Ingestion
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+load_dotenv()
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+async def main():
+    with DockerManager(stop_when_done=False) as m:
+        m.full_setup()
+        await DB_Manager.full_setup()
+        ingestion = Ingestion()
+        while True:
+            await ingestion.transcribe_next_meeting()
+            await ingestion.ingest_next_meeting()
+        
+        
+asyncio.run(main())
