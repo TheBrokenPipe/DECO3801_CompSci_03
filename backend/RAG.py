5750c07fbc27e16b1de90ba10a59008eb9fa9b06
ellinovak <39504423+ellinovak@users.noreply.github.com>
2024-10-11 19:25:19 +1000

    retriever that gets surrounding chunks for context and func to get sources list

--- backend/RAG.py
+++ backend/RAG.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from typing import Any, List, Tuple, Optional
+
+import sqlalchemy
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import logging
 
@@ -15,6 +19,8 @@ from langchain.docstore.document import Document
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_postgres import PGVector
+from sqlalchemy import SQLColumnExpression, cast, create_engine, delete, func, select, Integer
+from sqlalchemy.dialects.postgresql import JSON, JSONB, JSONPATH, UUID, insert
 
 
 class KeyPoints(BaseModel):
@@ -42,11 +48,11 @@ class RAG:
                 self.embeddings = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=1000)
             else:
                 self.embeddings = OllamaEmbeddings(model="nomic-embed-text")
-            
+
         else:
             self.llm = ChatOllama(model=os.getenv("OLLAMA_MODEL", "llama3.1e"), temperature=0.2)
             self.embeddings = OllamaEmbeddings(model="nomic-embed-text")
-        
+
         connection = f"postgresql+psycopg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('HOSTNAME')}:{os.getenv('PORT')}/{os.getenv('DB_NAME')}"
         self.logger.debug(connection)
 
@@ -59,13 +65,13 @@ class RAG:
 
     def invoke_llm(self, system_prompt: str, user_prompt: str) -> str:
         prompt = ChatPromptTemplate.from_messages(
-            [("system","{system_prompt}",),
-                ("human", "{user_prompt}"),])
+            [("system", "{system_prompt}",),
+             ("human", "{user_prompt}"), ])
         parser = StrOutputParser()
         session = prompt | self.llm | parser
         response = session.invoke({"system_prompt": system_prompt, "user_prompt": user_prompt})
         return response
-    
+
     @staticmethod
     def check_none(model: BaseModel) -> BaseModel:
         if model is None:
@@ -81,16 +87,15 @@ class RAG:
                 "If you do not know the value of an attribute asked to extract, "
                 "return null for the attribute's value.",
             ),
-            ("human","{text}")
+            ("human", "{text}")
         ])
         structured_llm = self.llm.with_structured_output(model) | RunnableLambda(self.check_none)
         session = prompt | structured_llm.with_retry()
         try:
-            response = session.invoke({"text":text})
+            response = session.invoke({"text": text})
         except Exception as e:
             response = None
         return response
-
 
     def abstract_summary_extraction(self, transcription):
         transcript = jsonl_to_txt(transcription)
@@ -114,13 +119,13 @@ class RAG:
             'key_points': self.key_points_extraction(transcription),
             'action_items': self.action_item_extraction(transcription),
         }
-    
+
     def embed_meeting(self, meeting: DB_Meeting, chunks: list[Document]):
         for chunk in chunks:
             if isinstance(self.embeddings, OllamaEmbeddings):
                 chunk.page_content = "search_document: " + chunk.page_content
             chunk.metadata["meeting_id"] = meeting.id
-        
+
         self.vector_store.add_documents(chunks)
 
     def format_docs(self, docs):
@@ -137,10 +142,10 @@ class RAG:
         prompt = ChatPromptTemplate.from_messages([("system", system_prompt,), ("human", "{question}")])
 
         qa_chain = (
-            {"context": retriever | self.format_docs, "question": RunnablePassthrough()}
-            | prompt
-            | self.llm
-            | StrOutputParser()
+                {"context": retriever | self.format_docs, "question": RunnablePassthrough()}
+                | prompt
+                | self.llm
+                | StrOutputParser()
         )
 
         return qa_chain.invoke(query_text)
@@ -155,6 +160,76 @@ class RAG:
         # Alternative prompts with context in user prompt rather than system prompt
         # system_prompt = "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, say that you don't know. Do not include any general information unless necessary. Use three sentences maximum and keep the answer concise."
         # user_prompt = f"Context: {context}\n\nQuestion: {query_text}""
-        
+
         return self.invoke_llm(system_prompt, user_prompt)
 
+    def get_sources_list(self, chunks: List[Document]) -> List[str]:
+        sources = []
+        for chunk in chunks:
+            transcript = chunk.metadata["filename"]
+
+            meeting_name = DB_Meeting.query.filter(DB_Meeting.meeting_transcript == transcript).first()
+            start_time: float = chunk.metadata["start_time"]
+            end_time: float = chunk.metadata["end_time"]
+            source_string = f"{meeting_name} at {start_time:.2f} - {end_time:.2f}"
+
+            sources.append(source_string)
+        return sources
+
+class DB_MeetingChunk(PGVector):
+    def _results_to_docs_and_scores(self, results: Any) -> List[Tuple[Document, float]]:
+        """Return docs and scores from results."""
+        docs = [
+            (
+                Document(
+                    id=str(result.EmbeddingStore.id),
+                    page_content=self.get_content_with_context(result, 2),
+                    metadata=result.EmbeddingStore.cmetadata,
+                ),
+                result.distance if self.embeddings is not None else None,
+            )
+            for result in results
+        ]
+        return docs
+
+    def get_content_with_context(self, chunk, n=2) -> str:
+        # n is number of chunks to get, if n=2 it will return the original with the 2 chunks above and 2 chunks below
+        with self._make_sync_session() as session:  # type: ignore[arg-type]
+            collection = self.get_collection(session)
+
+            chunk_filepath = chunk.EmbeddingStore.cmetadata["filename"]
+            chunk_id = chunk.EmbeddingStore.cmetadata["chunk_id"]
+
+            # create list of id's to get
+            ids = []
+            for i in range(chunk_id, chunk_id-n, -1):
+                ids.append(i)
+            ids.append(n)
+            for i in range(chunk_id-n, chunk_id):
+                ids.append(i)
+
+            # filter to get the ids of the filename from the
+            filter = {
+                "$and": [
+                    {"filename": {"$eq": chunk_filepath}},
+                    {"location": {"$in": ids}},
+                ]
+            }
+            filter_by = [self.EmbeddingStore.collection_id == collection.uuid, self._create_filter_clause(filter)]
+
+            consecutive_chunks: List[Any] = (
+                select(
+                    self.EmbeddingStore,
+                )
+                .filter(*filter_by)
+                .order_by(sqlalchemy.asc(cast(self.EmbeddingStore.cmetadata["chunk_id"].astext, Integer)))  # check
+                .join(
+                    self.CollectionStore,
+                    self.EmbeddingStore.collection_id == self.CollectionStore.uuid,)
+                .all()
+            )
+
+        # Concatenate all chunks into one string
+        doc_content = "\n".join(chunk.EmbeddingStore.document for chunk in consecutive_chunks)
+
+        return doc_content
